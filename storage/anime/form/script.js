99f5accb22921143684d53267c31577d11dde5a1
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2020-04-05 16:13:37 +0700

    optimize with js

---
+++ storage/anime/form/script.js
@@ -0,0 +1,30 @@
+/* 
+ * insert information
+ */
+
+//server1 ep switcher
+let ep1, ep2, ep3, ep4, ep5, ep6, ep7, ep8, ep9, ep10, ep11, ep12;
+ep1 = '<a class="button" href="S1E1.html">ep1</a> ';
+ep2 = '<a class="button" href="S1E2.html">ep2</a> ';
+ep3 = '<a class="button" href="S1E3.html">ep3</a> ';
+ep4 = '<a class="button" href="S1E4.html">ep4</a> ';
+ep5 = '<a class="button" href="S1E5.html">ep5</a> ';
+ep6 = '<a class="button" href="S1E6.html">ep6</a> ';
+ep7 = '<a class="button" href="S1E7.html">ep7</a> ';
+ep8 = '<a class="button" href="S1E8.html">ep8</a> ';
+ep9 = '<a class="button" href="S1E9.html">ep9</a> ';
+ep10 = '<a class="button" href="S1E10.html">ep10</a> ';
+ep11 = '<a class="button" href="S1E11.html">ep11</a> ';
+ep12 = '<a class="button" href="S1E12.html">ep12</a> ';
+
+/* 
+ * Hooker
+ */
+//Anime's info
+document.getElementById('ttle').innerHTML = '*Tên Anime*';
+document.getElementById('fansub').innerHTML = '';
+document.getElementById('linkd').innerHTML = '<a href="">LINK</a>'
+//server1
+document.getElementById('svname1').innerHTML = '*Tên server*:';
+document.getElementById('sev1').innerHTML = ep1 + ep2 + ep3 + ep4 + ep5 + ep6 + ep7 + ep8 + ep9 + ep10 + ep11 + ep12;
+
