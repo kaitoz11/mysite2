287e7474b132661d96d15dd975fd33a8c53e31e6
kaitozu <43519768+kaitoz11@users.noreply.github.com>
2020-04-27 02:22:28 +0700

    a

---
+++ storage/anime/strike-the-blood/script.js
@@ -0,0 +1,34 @@
+/* 
+ * insert information
+ */
+let inf = {
+    AnimeName: 'Strike the Blood',
+    fansubname: "YamiSora",
+    linkDownload: "https://www.yami-sora.org/strike-the-blood",
+    episodeLim: 24,
+}
+/* 
+ * Hooker
+ */
+//Anime's info
+document.title = inf.AnimeName;
+document.getElementById('ttle').innerHTML = inf.AnimeName;
+document.getElementById('fansub').innerHTML = inf.fansubname;
+document.getElementById('linkd').innerHTML = '<a href="'+inf.linkDownload+'">LINK</a>'
+//server1
+document.getElementById('svname1').innerHTML = 'YamiSora:';
+document.getElementById('sev1').innerHTML = epcepmenu(inf.episodeLim);
+//functions
+function epcepmenu(N) {
+    let i, Epssss = '', eptmp=0;
+    for(i=0;i<N;i++){
+        eptmp++;
+        Epssss = Epssss + '<a class="button" href="S1E'+eptmp+'.html">ep'+eptmp+'</a> ';
+    }
+    return Epssss;
+}
+
+/* 
+ *  add
+ */
+document.getElementById('navi').innerHTML = '<nav style="text-align: center; font-size: 16px; font-weight: bold;"><a class="button" style="border-radius: 14px" href="../../Anime.html">BACK</a></nav>';
